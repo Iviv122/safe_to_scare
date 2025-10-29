4b87c3d635ac4a31b3035d48ec4c80dafa806cbc
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-10-29 21:37:09 +0100

    better player light

--- objects/player/body/blicking_light.gd
+++ objects/player/body/blicking_light.gd
@@ -1,7 +1,6 @@
 extends PointLight2D
 class_name BlickingLight
 
-@export var speed : float = 0.1
 @export var magnitude : float = 0.2
 
 var init_scale : float 
@@ -9,14 +8,12 @@ var init_scale : float
 var tween := create_tween()
 
 func _ready():
-    init_scale = texture_scale
-    jump()
+	init_scale = texture_scale
 
 
-func jump() -> void:
-    if tween:
-        tween.kill()
-    tween = create_tween()
-    tween.tween_property(self,"texture_scale",init_scale+randf_range(-magnitude,magnitude),speed).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
-    await get_tree().create_timer(speed).timeout
-    jump()
+func _process(delta):
+	texture_scale = init_scale+randf_range(-magnitude,magnitude)
+	var r = randf_range(-0.1,0.1)
+	global_position = get_parent().global_position + Vector2(r,r)
+
+	
