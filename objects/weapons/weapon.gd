0c0835b7f3acd288b659aa68fb002f8ac914aafc
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-10-30 09:43:24 +0100

    self shoot fixx

--- objects/weapons/weapon.gd
+++ objects/weapons/weapon.gd
@@ -11,6 +11,11 @@ func shoot(_self : Node2D) -> void:
 	var mdir = (mpos-_self.global_position).normalized()
 
 	bul.global_position = _self.global_position + mdir*32
-	bul.dir = mdir
+
+	if mdir == Vector2.ZERO:
+		var r = randf_range(-3,3)
+		bul.dir = Vector2(sin(r),cos(r)) 
+	else:
+		bul.dir = mdir
 
 	_self.get_tree().current_scene.add_child(bul)
