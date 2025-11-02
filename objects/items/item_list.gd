07a96d775f9b0c9a16c8df7c9e4f8828b060f8a9
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-11-02 20:07:13 +0100

    fixed item loader

--- objects/items/item_list.gd
+++ objects/items/item_list.gd
@@ -5,14 +5,19 @@ class_name ItemsList
 
 
 static func get_list() -> Array[Item]:
-    var items: Array[Item]
+	var items: Array[Item]
 
-    var path = 'res://assets/items/'
-    var dir = DirAccess.open(path)
+	var path = 'res://assets/items/'
+	var dir = DirAccess.open(path)
 
-    for i in dir.get_files():
-        var file_path = path + i
-        var loaded = load(file_path)
-        items.append(loaded)
 
-    return items
+	for i in dir.get_files():
+		var file_path = path + i
+
+		if (file_path.get_extension() == "remap"):
+			file_path = file_path.replace('.remap', '')
+
+		var loaded = load(file_path)
+		items.append(loaded)
+
+	return items
